ddb1fe3bd51f57845f6ac431449f8e0c9898a05b
Electronic Devices Development Group <m.ishchenko@rmteam.ru>
2023-11-27 18:47:10 +0300

    Исправлена ошибка получения посылки. Написан буффер сообщения

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     window = ControlDataInLineEdit()
     window.mouseMoveEvent = window.mouseMoveEvent
     window.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec()) 
